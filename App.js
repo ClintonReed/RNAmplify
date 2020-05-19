c1579ed43ec0ed0fa499ff2a36355c364f33f264
Clinton Reed <CREED0043@my.sullivan.edu>
2020-05-19 11:02:34 -0400

    added authentication

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import {
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './src/graphql/mutations'
 import { listTodos } from './src/graphql/queries'
-
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 const initialState = { name: '', description: '' }
 
@@ -75,4 +75,4 @@ const styles = StyleSheet.create({
   todoName: { fontSize: 18 }
 })
 
-export default App
+export default withAuthenticator (App)
